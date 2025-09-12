2a1bb89b6bd858aeb59033c20780895fda371e9f
42060 <42060@LAPTOP-TSD4P3QV.home>
2025-09-12 16:41:55 +0200

    New WebHelp reponsive output

--- out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {18:0,10:9,12:9,6:-1,11:9,20:14,3:0,19:9,0:-1,4:0,2:0,9:-1,13:14,1:0,14:-1};
+var linkToParent = {20:10,19:0,5:-1,3:0,13:10,14:15,21:15,11:10,10:-1,15:-1,0:-1,4:0,2:0,1:0,12:10};
